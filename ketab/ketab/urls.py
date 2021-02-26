876ffb4772022875a3d9583cd237fe12f6530af0
rahim <rahim.aghareb@gmail.com>
2021-02-26 12:34:39 +0330

    translation: home page and dashboard

--- ketab/ketab/urls.py
+++ ketab/ketab/urls.py
@@ -17,14 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('rosetta/', include('rosetta.urls')),
     path('', include('books.urls')),
 
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
